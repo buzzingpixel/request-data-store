b9ee04fd9ec5b422815e681309c137a79218b4cf
TJ Draper <tj@buzzingpixel.com>
2019-01-14 15:51:17 -0600

    Initialize project

---
+++ .phpstorm.meta.php
@@ -0,0 +1,7 @@
+<?php
+namespace PHPSTORM_META;
+
+override(\corbomite\di\Di::get(0), map(['' => '@']));
+override(\corbomite\di\Di::getFromDefinition(0), map(['' => '@']));
+override(\corbomite\di\Di::make(0), map(['' => '@']));
+override(\corbomite\di\Di::makeFromDefinition(0), map(['' => '@']));
